89629dcd1e928300603e895c4a81996eebf6a4e4
Fathir <fathirbimashabri@gmail.com>
2023-03-27 16:23:36 +0700

    Setup Routes <333

---
+++ src/routes/health.ts
@@ -0,0 +1,6 @@
+import { Router, type Request, type Response, type NextFunction } from 'express'
+export const HealthRouter: Router = Router()
+
+HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: '200', data: 'Aku sayang kamu' })
+})
