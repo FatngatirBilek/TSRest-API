516508d5d0941c0bfa3d4235e08098f5647964c7
Fathir <fathirbimashabri@gmail.com>
2023-03-27 15:34:03 +0700

    Setup Eslint, Prettier, Husky

--- src/index.ts
+++ src/index.ts
@@ -7,4 +7,6 @@ app.use('/health', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({ status: '200', data: 'Aku sayang kamu' })
 })
 
-app.listen(port, () => { console.log(`server is listening on port ${port}`) })
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`)
+})
