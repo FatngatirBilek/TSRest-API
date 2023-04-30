5fa6d90600e3b0d1f2079c2fa432aeafabcb0e1f
Fatngatirbilek <fathirbimashabri@gmail.com>
2023-04-30 13:00:33 +0700

    Add Logger etc

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,6 +1,8 @@
 import { Router, type Request, type Response, type NextFunction } from 'express'
+import { logger } from '../utils/logger'
 export const HealthRouter: Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Health Check Success')
   res.status(200).send({ status: '200', data: 'Aku sayang kamu' })
 })
--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,6 +1,13 @@
 import { Router, type Request, type Response, type NextFunction } from 'express'
+import { logger } from '../utils/logger'
 export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Success Get Product Data')
   res.status(200).send({ status: true, statusCode: '200', data: [{ name: 'Sepatu Super', price: 500000 }] })
+})
+
+ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Success Add New Product Data')
+  res.status(200).send({ status: true, statusCode: '200', data: req.body })
 })
